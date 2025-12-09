ff54836e587c902909d6faf97266198df5937b26
Sufy55nS <22127928@bucks.ac.uk>
2025-12-09 17:05:57 +0000

    Update main.py

---
+++ src/main.py
@@ -0,0 +1,30 @@
+from contact import Contact
+from contact_book_hash import ContactBookHash
+from merge_sort import merge_sort
+
+
+def main():
+    book = ContactBookHash()
+
+    while True:
+        print("\n===== CONTACT BOOK =====")
+        print("1. Add Contact")
+        print("2. Search Contact")
+        print("3. Delete Contact")
+        print("4. Show All Contacts (Sorted)")
+        print("5. Exit")
+
+        choice = input("Enter choice: ")
+
+  def main():
+    book = ContactBookHash()
+
+    while True:
+        print("\n===== CONTACT BOOK =====")
+        print("1. Add Contact")
+        print("2. Search Contact")
+        print("3. Delete Contact")
+        print("4. Show All Contacts (Sorted)")
+        print("5. Exit")
+
+        choice = input("Enter choice: ")
