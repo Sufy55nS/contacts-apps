af80a4f5b8c0204a3cfaf89de852a7e682cfbd1c
Sufy55nS <22127928@bucks.ac.uk>
2025-12-09 17:03:51 +0000

    Update contact_book_hash.py

--- src/contact_book_hash.py
+++ src/contact_book_hash.py
@@ -5,3 +5,9 @@ class ContactBookHash:
 def __init__(self, size=20):
         self.size = size
         self.contacts = [[] for _ in range(size)]
+
+def _get_index(self, key):
+        total = 0
+        for char in key.lower():
+            total += ord(char)
+        return total % self.size
