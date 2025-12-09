bb1a97753850a560c95e6c28056d4961d8e395fb
Sufy55nS <22127928@bucks.ac.uk>
2025-12-09 16:58:56 +0000

    Update contact.py

--- src/contact.py
+++ src/contact.py
@@ -1,2 +1,9 @@
 class Contact:
     def __init__(self, name, phone, email=None, address=None, company=None, birthday=None):
+         self.name = name
+        self.phone = phone
+        self.email = email
+        self.address = address
+        self.company = company
+        self.birthday = birthday
+
