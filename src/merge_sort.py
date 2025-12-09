c64e1d8a557003ec3a09079d80a01739ae75a52e
Sufy55nS <22127928@bucks.ac.uk>
2025-12-09 17:10:01 +0000

    Update merge_sort.py

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -9,12 +9,15 @@ def merge_sort(contacts):
     return merge(left, right)
 
 
-def merge_sort(contacts):
-    if len(contacts) <= 1:
-        return contacts
+def merge(left, right):
+    sorted_list = []
+    i = j = 0
 
-    mid = len(contacts) // 2
-    left = merge_sort(contacts[:mid])
-    right = merge_sort(contacts[mid:])
+    while i < len(left) and j < len(right):
+        if left[i].name.lower() <= right[j].name.lower():
+            sorted_list.append(left[i])
+            i += 1
+        else:
+            sorted_list.append(right[j])
+            j += 1
 
-    return merge(left, right)
